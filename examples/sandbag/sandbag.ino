da6bd403997fb12f7a4387efa620b5fa03249e56
SimonGurney <simongurney@outlook.com>
2021-04-13 21:25:30 +0100

    Update sandbag.ino

--- examples/sandbag/sandbag.ino
+++ examples/sandbag/sandbag.ino
@@ -1,36 +1,30 @@
-//======================================================================
-// ライブラリ使用例2
-// ランダムな相手とバトル(Arduino側が必ず負ける)
-// バトルの仕様は初代のもの
-// PCのシリアルモニタから任意の文字を送信するとバトル開始
-// 2015/1/25 作成
-// 2020/6/28 DLinkOriginクラスを使用
-//======================================================================
 #include <DLink.h>
 
-DLinkOrigin dlink(2);
+DLinkOrigin dlink(6); //Put your GPIO number here
+DataSetOrigin senddata, recvdata;
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  DataSetOrigin senddata, recvdata;
-  // PCから任意の文字を受信
-  while (Serial.available() < 1);
-  Serial.read();
-  // 対戦相手をランダムに生成
+  Serial.println("===== Starting Battle =====");
   randomSeed(millis());
   senddata.field.species = (unsigned char)random(3, 15);
   senddata.field.effort  = 0;
-  senddata.field.victory = DLinkOrigin::LOSE;
+  senddata.field.victory = DLinkOrigin::LOSE; // Always lose
   senddata.field.version = DLinkOrigin::VER1;
-  // 育成ギアと通信
-  recvdata = dlink.battle(senddata);
-  // 受信フレームをPCに送信
-  Serial.println("===== Battle Success =====");
+  recvdata = dlink.battle(senddata, true, 1000); // Initiate battle every 1 second
+  if (recvdata.frame[0] == 0)
+  {
+    Serial.println("===== Battle Failure  ====="); // No response
+  }
+  else
+  {
+  Serial.println("===== Battle Success  ====="); // Battle initiated success
   Serial.print("frame[0] : 0b");
   Serial.println(recvdata.frame[0], BIN);
   Serial.print("frame[1] : 0b");
   Serial.println(recvdata.frame[1], BIN);
+  }
 }
